4e8cfa6c5ba12fae9b86f726b57a2fe3f58b01c6
Lalit Thapa <mlalitthapa@gmail.com>
2017-09-18 20:26:53 +0545

    setup controllers, models, migrations and seeders for threads and replies

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,25 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define("App\Models\Thread", function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory("App\User")->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
+
+$factory->define("App\Models\Reply", function (Faker $faker) {
+    return [
+        'thread_id' => function() {
+            return factory('App\Models\Thread')->create()->id;
+        },
+        'user_id' => function () {
+            return factory("App\User")->create()->id;
+        },
+        'body' => $faker->paragraph
+    ];
+});
