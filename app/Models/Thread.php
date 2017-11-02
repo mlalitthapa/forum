cdbed1e4206f7df0da8616efc7158e559a1a59fb
Lalit Thapa <mlalitthapa@gmail.com>
2017-11-02 21:11:28 +0545

    metadata for threads and pagination for replies

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -10,6 +10,15 @@ class Thread extends Model
 
     protected $guarded = [];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function ($builder) {
+            $builder->withCount('replies');
+        });
+    }
+
     /**
      * @return \Illuminate\Contracts\Routing\UrlGenerator|string
      */
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <a href="#">
@@ -16,20 +16,13 @@
                         {{ $thread->body }}
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                @foreach($thread->replies as $reply)
+                @foreach($replies as $reply)
                     @include('threads.reply')
                 @endforeach
-            </div>
-        </div>
 
+                {{ $replies->links() }}
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
                 @if(auth()->check())
                     <form action="{{ $thread->path() . '/replies' }}" method="POST">
                         {!! csrf_field() !!}
@@ -44,6 +37,16 @@
                         Please <a href="{{ route('login') }}">sign in</a> to participate in this thread.
                     </p>
                 @endif
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        This thread was published {{ $thread->created_at->diffForHumans() }} by
+                        <a href="#">{{ $thread->creator->name }}</a>, and currently has
+                        {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                    </div>
+                </div>
             </div>
         </div>
 
