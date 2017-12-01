2deb6bd15b0146bdf496431b7a64418fce50f642
Lalit Thapa <lalit@ekbana.com>
2017-12-01 10:59:21 +0545

    change default length for string in migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \Schema::defaultStringLength(191);
         view()->composer('*', function($view) {
             $channels = Cache::rememberForever('channels', function () {
                 return Channel::all();
